cc8d86063838d9cc6e1d2455ff94c6e0d1761b56
ijeremic <ijeremic89@gmail.com>
2024-02-21 11:32:53 +0100

    Project initialization

---
+++ src/test/java/serviceProvider/serviceProvider/ServiceProviderApplicationTests.java
@@ -0,0 +1,13 @@
+package serviceProvider.serviceProvider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceProviderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
