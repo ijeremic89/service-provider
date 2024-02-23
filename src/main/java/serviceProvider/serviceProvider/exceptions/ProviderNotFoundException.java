4b5b416ca249a904ebfd31c0eadff128edba99a0
ijeremic <ijeremic89@gmail.com>
2024-02-23 14:17:52 +0100

    Minor change in exception messages

--- src/main/java/serviceProvider/serviceProvider/exceptions/ProviderNotFoundException.java
+++ src/main/java/serviceProvider/serviceProvider/exceptions/ProviderNotFoundException.java
@@ -3,6 +3,6 @@ package serviceProvider.serviceProvider.exceptions;
 public class ProviderNotFoundException extends RuntimeException {
 
     public ProviderNotFoundException(Long id) {
-        super("Could not find provider with " + id + " ID");
+        super("Could not find provider with Id: " + id);
     }
 }
--- src/main/java/serviceProvider/serviceProvider/exceptions/ServiceNotFoundException.java
+++ src/main/java/serviceProvider/serviceProvider/exceptions/ServiceNotFoundException.java
@@ -3,6 +3,6 @@ package serviceProvider.serviceProvider.exceptions;
 public class ServiceNotFoundException extends RuntimeException {
 
     public ServiceNotFoundException(Long id) {
-        super("Could not find service with " + id + " ID");
+        super("Could not find service with Id: " + id);
     }
 }
