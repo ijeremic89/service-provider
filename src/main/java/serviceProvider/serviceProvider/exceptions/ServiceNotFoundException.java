3cafb63ff68c3da420cdeaacd2eafb836f5c39db
ijeremic <ijeremic89@gmail.com>
2024-02-23 10:26:07 +0100

    Add ServiceNotFoundException and refactor ProviderServiceImpl

---
+++ src/main/java/serviceProvider/serviceProvider/exceptions/ServiceNotFoundException.java
@@ -0,0 +1,8 @@
+package serviceProvider.serviceProvider.exceptions;
+
+public class ServiceNotFoundException extends RuntimeException {
+
+    public ServiceNotFoundException(Long id) {
+        super("Could not find service with " + id + " ID");
+    }
+}
